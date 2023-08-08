2821bec2de880968c7beb7d765fdeaf45dc9b8f4
Hope G <119355967+Ho0pHo0p@users.noreply.github.com>
2023-08-08 16:08:24 -0400

    Menu appears when menu button clicked

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const menuBtn= document.querySelector('.menu-btn');
 const learnMoreBtn = document.querySelector('.learn-more-btn')
 const viewFlavorsBtn = document.querySelector('.view-flavor-btn')
+const menu = document.querySelector('.menu-cont');
+const signup = document.querySelector('.sign-up');
+const signupInput = document.querySelector('.sign-up-input')
 
 const toggleMenu = () => {
-  
+  menu.classList.toggle('hidden')
 }
 
-
-menuBtn.addEventListener('click', )
+menuBtn.addEventListener('click', toggleMenu )
+signup.addEventListener('submit', (e) => {
+  e.preventDefault();
+  signupInput.value=""
+} )

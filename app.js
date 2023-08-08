191849d3250dd875aacf7083f21635e68448dc44
Hope G <119355967+Ho0pHo0p@users.noreply.github.com>
2023-08-07 20:42:02 -0400

    Update icon

---
+++ app.js
@@ -0,0 +1,10 @@
+const menuBtn= document.querySelector('.menu-btn');
+const learnMoreBtn = document.querySelector('.learn-more-btn')
+const viewFlavorsBtn = document.querySelector('.view-flavor-btn')
+
+const toggleMenu = () => {
+  
+}
+
+
+menuBtn.addEventListener('click', )
